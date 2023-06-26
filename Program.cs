012a833aad876b2ce7ad5a263342941b74b8f2cd
Anandmohanty <mohantyanand3@gmail.com>
2023-06-26 08:06:54 +0530

    [Anand] - Instance Varibles

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Variables marksobj1 = new Variables();
+            marksobj1.mathMarks = 50;
+            marksobj1.engMarks = 90;
+            
             Console.WriteLine("Welcome to type of variables");
+            Console.WriteLine(marksobj1.engMarks);
+            Console.WriteLine(marksobj1.mathMarks);
         }
     }
 }
---
+++ Variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeOfVariables
+{
+    public class Variables
+    {
+        public int mathMarks; //These variables are instance variables
+        public int engMarks; //These variables are instance variables
+       
+       
+       
+      
+
+    }
+}
+
