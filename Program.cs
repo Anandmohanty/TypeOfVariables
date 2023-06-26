2ea7d23d1e2fb387fcf4f90ab0b6c34f8b926c65
Anandmohanty <mohantyanand3@gmail.com>
2023-06-26 07:45:58 +0530

    [Anand] - Welcome to types of variables

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypeOfVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to type of variables");
+        }
+    }
+}
